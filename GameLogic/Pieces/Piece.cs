c12a010ce34a141f6d57d29223aa4a5dd0fc0317
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-15 18:04:27 +0200

    Piece class added

---
+++ GameLogic/Pieces/Piece.cs
@@ -0,0 +1,25 @@
+namespace LeadersBoardGame.Pieces;
+
+using LeadersBoardGame.Players;
+
+public class Piece
+{
+    public int Id { get; init; }
+    public PlayerColor Color { get; init; }
+    public PieceKind Kind { get; set; }
+
+    public Piece(int id, PlayerColor color, PieceKind kind)
+    {
+        Id = id;
+        Color = color;
+        Kind = kind;
+    }
+
+    /// <summary>
+    /// Compare the piece with another to check if they share the same color and kind
+    /// </summary>
+    public bool IsSimilar(Piece otherPiece)
+    {
+        return Color == otherPiece.Color && Kind == otherPiece.Kind;
+    }
+}
