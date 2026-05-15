61fd98d0f4f38747dc83664a29eed31e37fa136b
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-15 19:18:57 +0200

    Position class added

---
+++ GameLogic/Boards/Position.cs
@@ -0,0 +1,18 @@
+namespace LeadersBoardGame.Boards;
+
+public class Position
+{
+    // We use byte for coordinates since there are only 37 different positions on the board
+    public byte X { get; init; }
+    public byte Y { get; init; }
+
+    public Position(byte x, byte y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Position(Position refPosition) : this(refPosition.X, refPosition.Y)
+    {
+    }
+}
