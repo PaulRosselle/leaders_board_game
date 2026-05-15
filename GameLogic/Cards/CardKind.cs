51baffb1e2063bd926f7f53a1ce90c411dbb68e3
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-15 12:40:04 +0200

    PieceKind enum added

--- GameLogic/Cards/CardKind.cs
+++ GameLogic/Cards/CardKind.cs
@@ -26,40 +26,34 @@ public enum CardKind
 
 public static class CardKindMethods
 {
-    public static AbilityKind[] GetAbilityKinds(this CardKind cardKind)
+    public static AbilityKind[] GetAbilityKinds(this CardKind cardKind) => cardKind switch
     {
-        return cardKind switch
-        {
-            CardKind.Acrobat => new AbilityKind[] { AbilityKind.Active },
-            CardKind.Archer => new AbilityKind[] { AbilityKind.Passive },
-            CardKind.Assassin => new AbilityKind[] { AbilityKind.Passive },
-            CardKind.Brewmaster => new AbilityKind[] { AbilityKind.Active },
-            CardKind.Bruiser => new AbilityKind[] { AbilityKind.Active },
-            CardKind.ClawLauncher => new AbilityKind[] { AbilityKind.Active },
-            CardKind.HermitAndCub => new AbilityKind[] { AbilityKind.Special },
-            CardKind.Illusionist => new AbilityKind[] { AbilityKind.Active },
-            CardKind.Jailer => new AbilityKind[] { AbilityKind.Passive },
-            CardKind.LeaderKing or CardKind.LeaderQueen => [],
-            CardKind.Manipulator => new AbilityKind[] { AbilityKind.Active },
-            CardKind.Nemesis => new AbilityKind[] { AbilityKind.Special },
-            CardKind.Protector => new AbilityKind[] { AbilityKind.Passive },
-            CardKind.Rider => new AbilityKind[] { AbilityKind.Active },
-            CardKind.RoyalGuard => new AbilityKind[] { AbilityKind.Active },
-            CardKind.Vizier => new AbilityKind[] { AbilityKind.Passive },
-            CardKind.Wanderer => new AbilityKind[] { AbilityKind.Active },
-            _ => throw new InvalidOperationException($"No ability kinds found for card kind {cardKind}"),
-        };
-    }
+        CardKind.Acrobat => new AbilityKind[] { AbilityKind.Active },
+        CardKind.Archer => new AbilityKind[] { AbilityKind.Passive },
+        CardKind.Assassin => new AbilityKind[] { AbilityKind.Passive },
+        CardKind.Brewmaster => new AbilityKind[] { AbilityKind.Active },
+        CardKind.Bruiser => new AbilityKind[] { AbilityKind.Active },
+        CardKind.ClawLauncher => new AbilityKind[] { AbilityKind.Active },
+        CardKind.HermitAndCub => new AbilityKind[] { AbilityKind.Special },
+        CardKind.Illusionist => new AbilityKind[] { AbilityKind.Active },
+        CardKind.Jailer => new AbilityKind[] { AbilityKind.Passive },
+        CardKind.LeaderKing or CardKind.LeaderQueen => [],
+        CardKind.Manipulator => new AbilityKind[] { AbilityKind.Active },
+        CardKind.Nemesis => new AbilityKind[] { AbilityKind.Special },
+        CardKind.Protector => new AbilityKind[] { AbilityKind.Passive },
+        CardKind.Rider => new AbilityKind[] { AbilityKind.Active },
+        CardKind.RoyalGuard => new AbilityKind[] { AbilityKind.Active },
+        CardKind.Vizier => new AbilityKind[] { AbilityKind.Passive },
+        CardKind.Wanderer => new AbilityKind[] { AbilityKind.Active },
+        _ => throw new InvalidOperationException($"No ability kinds found for card kind {cardKind}"),
+    };
 
-    public static LeaderKind? GetLeaderKind(this CardKind cardKind)
+    public static LeaderKind? GetLeaderKind(this CardKind cardKind) => cardKind switch
     {
-        return cardKind switch
-        {
-            CardKind.LeaderKing => LeaderKind.King,
-            CardKind.LeaderQueen => LeaderKind.Queen,
-            _ => null,
-        };
-    }
+        CardKind.LeaderKing => LeaderKind.King,
+        CardKind.LeaderQueen => LeaderKind.Queen,
+        _ => null,
+    };
 
     public static bool IsLeader(this CardKind cardKind)
     {
