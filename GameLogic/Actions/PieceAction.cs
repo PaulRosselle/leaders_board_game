2d636b82ef19d2bc4d5d9104e8f0f1a7f205c459
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-16 08:05:09 +0200

    Tile class added

--- GameLogic/Actions/PieceAction.cs
+++ GameLogic/Actions/PieceAction.cs
@@ -8,15 +8,15 @@ public class PieceAction
     public PieceActionKind Kind { get; init; }
     public Piece SourcePiece { get; init; }
     private Position? _sourceOriginPos;
-    public Position? SourceOriginPos { get => _sourceOriginPos; set => _sourceOriginPos = value is null ? null : value; }
+    public Position? SourceOriginPos { get => _sourceOriginPos; set => _sourceOriginPos = value is null ? null : new Position(value); }
     private Position? _sourceDestPos;
-    public Position? SourceDestPos { get => _sourceDestPos; set => _sourceDestPos = value is null ? null : value; }
+    public Position? SourceDestPos { get => _sourceDestPos; set => _sourceDestPos = value is null ? null : new Position(value); }
     private Piece? _targetPiece;
-    public Piece? TargetPiece { get => _targetPiece; set => _targetPiece = value is null ? null : value; }
+    public Piece? TargetPiece { get => _targetPiece; set => _targetPiece = value is null ? null : new Piece(value); }
     private Position? _targetOriginPos;
-    public Position? TargetOriginPos { get => _targetOriginPos; set => _targetOriginPos = value is null ? null : value; }
+    public Position? TargetOriginPos { get => _targetOriginPos; set => _targetOriginPos = value is null ? null : new Position(value); }
     private Position? _targetDestPos;
-    public Position? TargetDestPos { get => _targetDestPos; set => _targetDestPos = value is null ? null : value; }
+    public Position? TargetDestPos { get => _targetDestPos; set => _targetDestPos = value is null ? null : new Position(value); }
 
     public PieceAction(PieceActionKind kind, Piece sourcePiece)
     {
