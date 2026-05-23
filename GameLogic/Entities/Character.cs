987b71f80b71847cf376ad3c021f94e1061dfb21
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-23 09:03:50 +0200

    Piece class refactored to Character

--- GameLogic/Entities/Character.cs
+++ GameLogic/Entities/Character.cs
@@ -1,1 +1,21 @@
+using LeadersBoardGame.GameLogic.Enums;
+
 namespace LeadersBoardGame.GameLogic.Entities;
+
+public class Character
+{
+    public int Id { get; }
+    public CharacterType Type { get; set; }
+    public TeamColor Color { get; set; }
+
+    public Character(int id, CharacterType type, TeamColor color)
+    {
+        Id = id;
+        Color = color;
+        Type = type;
+    }
+
+    public Character(Character refCharacter) : this(refCharacter.Id, refCharacter.Type, refCharacter.Color)
+    {
+    }
+}
