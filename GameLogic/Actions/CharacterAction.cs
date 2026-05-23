1e50e45e33c4091c3bc46453be52c17cc9be0db0
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-23 11:18:25 +0200

    PieceAction class refactored to CharacterAction

--- GameLogic/Actions/CharacterAction.cs
+++ GameLogic/Actions/CharacterAction.cs
@@ -1,1 +1,20 @@
+using System.Collections.Generic;
+using LeadersBoardGame.GameLogic.Enums;
+
 namespace LeadersBoardGame.GameLogic.Actions;
+
+public class CharacterAction : IGameAction
+{
+    public GameActionType ActionType => GameActionType.CharacterAction;
+
+    public bool IsActiveAbility { get; }
+    public CharacterActionDetail Source { get; }
+    public List<CharacterActionDetail> Targets { get; }
+
+    public CharacterAction(CharacterActionDetail source, List<CharacterActionDetail> targets, bool isActiveAbility)
+    {
+        Source = source;
+        Targets = targets;
+        IsActiveAbility = isActiveAbility;
+    }
+}
