a90223820789446b2b8bf049a37df9ca7fc2836e
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-23 09:06:27 +0200

    Tile class refactored to Cell

--- GameLogic/Entities/Cell.cs
+++ GameLogic/Entities/Cell.cs
@@ -1,1 +1,17 @@
 namespace LeadersBoardGame.GameLogic.Entities;
+
+public class Cell
+{
+    public Position Pos { get; }
+    public Character? Character { get; set; }
+
+    public Cell(Position pos)
+    {
+        Pos = pos;
+    }
+
+    public Cell(Cell refCell) : this(new Position(refCell.Pos))
+    {
+        Character = refCell.Character is null ? null : new Character(refCell.Character);
+    }
+}
