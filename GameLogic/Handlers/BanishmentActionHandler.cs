b399c57ce97b9b5cd15430f57885c6e995ff1a3e
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-25 16:40:17 +0200

    BanishmentActionHandler added

--- GameLogic/Handlers/BanishmentActionHandler.cs
+++ GameLogic/Handlers/BanishmentActionHandler.cs
@@ -1,1 +1,29 @@
+using LeadersBoardGame.GameLogic.Actions;
+using LeadersBoardGame.GameLogic.Entities;
+
 namespace LeadersBoardGame.GameLogic.Handlers;
+
+public class BanishmentActionHandler : IActionHandler
+{
+    private Game _game;
+    public Game Game => _game;
+    public BanishmentAction Action { get; }
+
+    public BanishmentActionHandler(Game game, BanishmentAction action)
+    {
+        _game = game;
+        Action = action;
+    }
+
+    public void DoAction()
+    {
+        _game.RecruitableCards.Remove(Action.Card);
+        _game.History.Add(Action);
+    }
+
+    public void UndoAction()
+    {
+        _game.RecruitableCards.Add(Action.Card);
+        _game.History.Remove(Action);
+    }
+}
