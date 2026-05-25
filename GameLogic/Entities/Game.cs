0a716c83be34c532d44d4dde967dd7cb4f270d6a
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-25 17:07:31 +0200

    RecruitmentActionHandler added

--- GameLogic/Entities/Game.cs
+++ GameLogic/Entities/Game.cs
@@ -11,19 +11,21 @@ public class Game
     public Player CurrentPlayer { get; set; }
     public TurnPhase CurrentPhase {get; set; }
     public Board Board { get; }
+    public GameMode GameMode { get; }
     public List<CharacterCard> RecruitableCards { get; }
     public Dictionary<Character, CharacterState> CharacterStates { get; }
     public List<IGameAction> History;
 
-    public Game(Player[] players, Player firstPlayer, Player currentPlayer, TurnPhase currentPhase, Board board, 
-                List<CharacterCard> recruitableCards, Dictionary<Character, CharacterState> characterStates, 
-                List<IGameAction> history)
+    public Game(Player[] players, Player firstPlayer, Player currentPlayer, TurnPhase currentPhase,
+                Board board, GameMode gameMode, List<CharacterCard> recruitableCards, 
+                Dictionary<Character, CharacterState> characterStates, List<IGameAction> history)
     {
         Players = players;
         FirstPlayer = firstPlayer;
         CurrentPlayer = currentPlayer;
         CurrentPhase = currentPhase;
         Board = board;
+        GameMode = gameMode;
         RecruitableCards = recruitableCards;
         CharacterStates = characterStates;
         History = history;
--- GameLogic/Handlers/RecruitmentActionHandler.cs
+++ GameLogic/Handlers/RecruitmentActionHandler.cs
@@ -1,1 +1,50 @@
+using LeadersBoardGame.GameLogic.Actions;
+using LeadersBoardGame.GameLogic.Entities;
+using LeadersBoardGame.GameLogic.Enums;
+using LeadersBoardGame.GameLogic.Queries;
+
 namespace LeadersBoardGame.GameLogic.Handlers;
+
+public class RecruitmentActionHandler : IActionHandler
+{
+    private Game _game;
+    public Game Game => _game;
+    public RecruitmentAction Action { get; }
+
+    public RecruitmentActionHandler(Game game, RecruitmentAction action)
+    {
+        _game = game;
+        Action = action;
+    }
+
+    public void DoAction()
+    {
+        // We add the recruited character to its destination on the board (which must be empty)
+        BoardQuery.GetCell(Game.Board, Action.DestPos).Character = Action.Character;
+        // Then we remove immediately the character card from the recruitable cards
+        CharacterCard card = Action.Character.CharacterType.GetCharacterCard();
+        if (_game.RecruitableCards.Contains(card))
+        {
+            _game.RecruitableCards.Remove(card);
+        }
+
+        _game.History.Add(Action);
+    }
+
+    public void UndoAction()
+    {
+        // We empty the character destination on the board since cancelling a
+        // recruitment goes with removing the correspond character on the board
+        BoardQuery.GetCell(Game.Board, Action.DestPos).Character = null;
+        // We add back the character from the recruitable cards if needed
+        CharacterCard card = Action.Character.CharacterType.GetCharacterCard();
+        if (!_game.RecruitableCards.Contains(card))
+        {
+            // Since cards are picked from the start of the list in Discovery
+            // mode, we add back the recruited card at index zero
+            _game.RecruitableCards.Insert(0, card);
+        }
+
+        _game.History.Remove(Action);
+    }
+}
