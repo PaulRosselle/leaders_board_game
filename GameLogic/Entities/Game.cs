586d1a9741ab1e868d9c16b4f6f2cebeaa807b52
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-28 10:04:31 +0200

    TurnPhase removed

--- GameLogic/Entities/Game.cs
+++ GameLogic/Entities/Game.cs
@@ -9,20 +9,18 @@ public class Game
     public Player[] Players { get; }
     public Player FirstPlayer { get; }
     public Player CurrentPlayer { get; set; }
-    public TurnPhase CurrentPhase {get; set; }
     public Board Board { get; }
     public GameMode GameMode { get; }
     public List<CharacterCard> RecruitableCards { get; }
     public List<IGameAction> History;
 
-    public Game(Player[] players, Player firstPlayer, Player currentPlayer, TurnPhase currentPhase,
+    public Game(Player[] players, Player firstPlayer, Player currentPlayer, 
                 Board board, GameMode gameMode, List<CharacterCard> recruitableCards, 
                 List<IGameAction> history)
     {
         Players = players;
         FirstPlayer = firstPlayer;
         CurrentPlayer = currentPlayer;
-        CurrentPhase = currentPhase;
         Board = board;
         GameMode = gameMode;
         RecruitableCards = recruitableCards;
