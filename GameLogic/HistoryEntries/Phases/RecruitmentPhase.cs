1eb02ee8c3b0adb595a18adac26f21d052693091
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-28 10:22:16 +0200

    HistoryEntries phases classes added

---
+++ GameLogic/HistoryEntries/Phases/RecruitmentPhase.cs
@@ -0,0 +1,13 @@
+namespace LeadersBoardGame.GameLogic.HistoryEntries.Phases;
+
+using System.Collections.Generic;
+using LeadersBoardGame.GameLogic.Actions;
+
+public class RecruitmentPhase : IPhase, ISegment
+{
+    public List<IGameAction> Actions => throw new System.NotImplementedException();
+
+    public IGameAction StartAction => throw new System.NotImplementedException();
+
+    public IGameAction EndAction => throw new System.NotImplementedException();
+}
