a4f0fa60c9e9fdcf53e5fe4a121175e320e6181c
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-29 07:58:46 +0200

    GameHistory class implemented

--- GameLogic/Entities/GameHistory.cs
+++ GameLogic/Entities/GameHistory.cs
@@ -1,11 +1,17 @@
 namespace LeadersBoardGame.GameLogic.Entities;
 
 using System;
+using System.Collections.Generic;
+using LeadersBoardGame.GameLogic.HistoryEntries;
 
 public class GameHistory
 {
-    public GameHistory()
+    public GameConfig Config { get; }
+    public List<IHistoryEntry> Entries { get; }
+
+    public GameHistory(GameConfig config, List<IHistoryEntry> entries)
     {
-        throw new NotImplementedException();    
+        Config = config;
+        Entries = entries;
     }
 }
