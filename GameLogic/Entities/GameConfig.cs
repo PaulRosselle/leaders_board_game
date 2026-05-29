19c181e565bfe94cc88506bc7667b352cb5f43a0
Paul Rosselle <101338905+PaulRosselle@users.noreply.github.com>
2026-05-29 07:56:19 +0200

    GameConfig class implemented

--- GameLogic/Entities/GameConfig.cs
+++ GameLogic/Entities/GameConfig.cs
@@ -1,11 +1,25 @@
 namespace LeadersBoardGame.GameLogic.Entities;
 
 using System;
+using LeadersBoardGame.GameLogic.Actions;
+using LeadersBoardGame.GameLogic.Enums;
+
 
 public class GameConfig
 {
-    public GameConfig()
+    public Player[] Players { get; }
+    public Player FirstPlayer { get; }
+    public GameMode GameMode { get; }
+    public CharacterCard[] InitialRecruitableCards { get; }
+    public IGameAction[] InitialPlacements { get; }
+
+    public GameConfig(Player[] players, Player firstPlayer, GameMode gameMode,
+                      CharacterCard[] initialRecruitableCards, IGameAction[] initialPlacements)
     {
-        throw new NotImplementedException();    
+        Players = players;
+        FirstPlayer = firstPlayer;
+        GameMode = gameMode;
+        InitialRecruitableCards = initialRecruitableCards;
+        InitialPlacements = initialPlacements;
     }
 }
